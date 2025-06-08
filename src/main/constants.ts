1f3919c09b2774158c3286b5e4e3d4765b4d8a3d
Jelly-Fish119 <skmoinuddin0800@gmail.com>
2025-06-08 16:05:00 +0300

    first build success

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -1,1 +1,22 @@
-0000000000000000000000000000000000000000
+import * as path from 'path';
+
+const lulumiRootPath = process.env.NODE_ENV === 'development'
+  ? path.resolve(__dirname, '../../')
+  : path.resolve(__dirname, '../');
+const lulumiHelperPath = process.env.NODE_ENV === 'development'
+  ? path.resolve(lulumiRootPath, 'src', 'helper')
+  : path.resolve(lulumiRootPath, 'dist');
+const lulumiPreloadPath = process.env.NODE_ENV === 'development'
+  ? `http://localhost:${require('../../.electron-vue/config').port}`
+  : path.resolve(lulumiRootPath, 'dist');
+
+export default {
+  lulumiRootPath,
+  lulumiHelperPath,
+  lulumiPreloadPath,
+  devUserData: `${path.resolve(lulumiRootPath, 'userData')}`,
+  testUserData: `${path.resolve(lulumiRootPath, 'test', 'userData')}`,
+  lulumiPagesCustomProtocol: 'lulumi',
+  lulumiPDFJSPath: `${path.resolve(lulumiHelperPath, 'pdfjs')}`,
+  lulumiRev: '0587a3f856bc9c239a664c8a27573df3bfd5c4d1',
+};
